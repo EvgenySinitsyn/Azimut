0a7352cb5221a77e91abdd519ec156bc48493ed8
Daria Pavluhina <d.pavluhina@mail.ru>
2023-10-23 16:19:11 +0300

    Task-2 Приложение, обработчик гл. страницы, БД, модели, суперюзер

--- azimut/azimut/settings.py
+++ azimut/azimut/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reports.apps.ReportsConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'azimut.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reports_db',
+        'USER': 'root',
+        'PASSWORD': '1234512345',
+        'HOST': 'localhost',
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
