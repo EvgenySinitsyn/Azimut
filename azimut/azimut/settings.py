b9f7a29557181a9e30eba5cd7c66c81f2be50405
EvgenySinitsyn <looneystop@mail.ru>
2023-11-09 20:03:05 +0300

    fix

--- azimut/azimut/settings.py
+++ azimut/azimut/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@o1$*moc-52rkqv)w*&$_yorgnpv+urf7)!mq)+*7qs9#05m%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
